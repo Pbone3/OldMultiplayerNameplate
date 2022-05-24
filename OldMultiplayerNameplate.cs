a6a3cf7673192c1969f87ad48e3bdfc450722f52
Pbone3 <pbone785@gmail.com>
2022-05-24 17:27:36 -0500

    v1.0.2

--- OldMultiplayerNameplate.cs
+++ OldMultiplayerNameplate.cs
@@ -1,13 +1,23 @@
+using System;
 using Terraria;
+using Terraria.GameContent.UI;
 using Terraria.ModLoader;
 
 namespace OldMultiplayerNameplate
 {
 	public class OldMultiplayerNameplate : Mod
 	{
+        private Type _oldNameplateType;
+
         public override void Load()
         {
+            _oldNameplateType = Main.ActiveClosePlayersTeamOverlay.GetType();
             Main.ActiveClosePlayersTeamOverlay = new OneThreeMultiplayerClosePlayersOverlay();
+        }
+
+        public override void Unload()
+        {
+            Main.ActiveClosePlayersTeamOverlay = (IMultiplayerClosePlayersOverlay)Activator.CreateInstance(_oldNameplateType);
         }
     }
 }
